630f38dcac80898bf9d1243bedf83f15ed5146df
Andrey <zuborev.kh@gmail.com>
2020-06-02 00:27:20 +0300

    prof

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::get('/', 'SiteController@index')->name('index');
 
 Auth::routes();
 
